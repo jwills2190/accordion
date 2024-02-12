4093d641382f13a862669322bad53be81f56f173
Joel Williams <joel.williams90@gmail.com>
2024-02-12 12:05:29 -0500

    updated and improved readability

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Accordion from './components/accordion';
+import RandomColor from './components/random-color';
 // import React, { useState } from 'react';
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       {/* Accordion component */}
       <Accordion />
+      <RandomColor />
     </div>
   );
 }
